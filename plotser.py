10b9135175a234f97a949ee168e245c3ebe052db
thijsh <github@thd.ninja>
2021-01-24 22:15:15 +0100

    Tested with plotser first build and made some tweaks (smaller steps, more accuracy)

--- plotser.py
+++ plotser.py
@@ -7,12 +7,15 @@ import sys
 import atexit
 
 # Dimensions of plotter in mm (from stepper motors axes)
-width = 1000
-height = 1000
-min_x = 100
-min_y = 100
-max_x = 900
-max_y = 900
+width = 850
+height = 655
+paper_width = 420
+paper_height = 297
+paper_margin = 50
+min_x = (width - paper_width) / 2 + paper_margin
+min_y = (height - paper_height) / 2 + paper_margin
+max_x = width - min_x
+max_y = height - min_y
 grid_small = 10
 grid_large = 50
 grid_background = "gray90" 
@@ -26,6 +29,7 @@ line_done_color = "magenta2"
 spool_diameter = 30
 cycle_mm = math.pi * spool_diameter / 512 # 0.184078 mm
 step_mm = cycle_mm / 8 # 0.023 mm
+divide_line_steps = cycle_mm * 3
 
 steppers = [
   [12, 16, 18, 22], # GPIO 18, 23, 24, 25
@@ -46,59 +50,20 @@ steps = [
 ]
 
 substep = [0, 0, 0, 0]
-stepper_rate = 1500
+stepper_rate = 1500 # 1500 is safe
 stepper_delay = 1.0 / stepper_rate
 
-# Test drawing
-spiral_factor = 50.0
-spiral_scale = 0.1
-points = []
-print("Calculating stepper motor movement")
-for i in range(0, 10000):
-  # todo: improve bounding box so line point moves to intersect instead of just min/max
-  points.append([ 
-    max(min(math.sin(i / spiral_factor) * i * spiral_scale + width / 2, max_x), min_x),
-    max(min(math.cos(i / spiral_factor) * i * spiral_scale + height / 2, max_x), min_x),
-  ])
-
-# Set up GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-for pins in steppers:
-  for pin in pins:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, 0)
-
-# Set up clean exit
-def exit_handler():
-  print("Returning to home point")
-  target = pythagoras([width/2, height/2])
-  move_to(pos, target)
-  print("Turning off stepper motors")
-  GPIO.cleanup()
-atexit.register(exit_handler)
-
-# Init GUI
-try:
-  gui = tk.Tk()
-except:
-  print("GUI disabled, running headless")
-if gui:
-  print("GUI enabled, drawing stepper motor movement")
-  canvas = tk.Canvas(gui, width=width, height=height)
-  canvas.config(background=grid_background)
-  canvas.pack()
-  for x in range(0, width, grid_small):
-    canvas.create_line(x, 0, x, height, fill=grid_large_color if x % grid_large == 0 else grid_small_color)
-  for y in range(0, height, grid_small):
-    canvas.create_line(0, y, width, y, fill=grid_large_color if y % grid_large == 0 else grid_small_color)
-  for i in range(len(points) - 1):
-    canvas.create_line(points[i][0], height - points[i][1], points[i+1][0], height - points[i+1][1], fill=line_color)
-  canvas.create_line(min_x - 1, min_y - 1, max_x + 1, min_y - 1, fill=line_color, dash=(3,5), width=2)
-  canvas.create_line(min_x - 1, min_y - 1, min_x - 1, max_y + 1, fill=line_color, dash=(3,5), width=2)
-  canvas.create_line(max_x + 1, max_y + 1, max_x + 1, min_y - 1, fill=line_color, dash=(3,5), width=2)
-  canvas.create_line(max_x + 1, max_y + 1, min_x - 1, max_y + 1, fill=line_color, dash=(3,5), width=2)
-  gui.update()
+def pythagoras(xy):
+  b = xy[1] ** 2
+  l = xy[0] ** 2
+  t = (height - xy[1]) ** 2
+  r = (width - xy[0]) ** 2
+  return [
+    math.sqrt(b + l),
+    math.sqrt(l + t),
+    math.sqrt(t + r),
+    math.sqrt(r + b),
+  ]
 
 def move_cycle(stepper, speed):
   for i in range(abs(speed)):
@@ -122,25 +87,19 @@ def get_directions(pos, target):
     )
   return directions
 
-def pythagoras(xy):
-  b = xy[1] ** 2
-  l = xy[0] ** 2
-  t = (height - xy[1]) ** 2
-  r = (width - xy[0]) ** 2
-  return [
-    math.sqrt(b + l),
-    math.sqrt(l + t),
-    math.sqrt(t + r),
-    math.sqrt(r + b),
-  ]
-
 def move_to(pos, target):
-  directions = get_directions(pos, target)
-  while directions != [0, 0, 0, 0]:
-    for d in range(4):
-      pos[d] += cycle_mm * directions[d]
-    move_all(directions)
-    directions = get_directions(pos, target)
+  substeps = int((abs(pos[0] - target[0]) + abs(pos[1] - target[1])) / divide_line_steps) + 1
+  print "Moving in substeps:", substeps
+  for i in range(substeps):
+    subtarget = []
+    for stepper in range(4):
+      subtarget.append(pos[stepper] + (target[stepper] - pos[stepper]) / substeps * i)
+    directions = get_directions(pos, subtarget)
+    while directions != [0, 0, 0, 0]:
+      for d in range(4):
+        pos[d] += cycle_mm * directions[d]
+      move_all(directions)
+      directions = get_directions(pos, subtarget)
 
 def move_all(directions):
   for step in range(8):
@@ -149,6 +108,59 @@ def move_all(directions):
         for pin in range(4):
           GPIO.output(steppers[stepper][pin], steps[ step if directions[stepper]==1 else 7 - step ][pin])
     time.sleep(stepper_delay)
+
+# Test drawing
+spiral_factor = 75.0
+spiral_scale = 0.05
+points = []
+print("Calculating stepper motor movement")
+for i in range(0, 10000):
+  # todo: improve bounding box so line point moves to intersect instead of just min/max
+  points.append([ 
+    max(min(math.sin(i / spiral_factor) * i * spiral_scale + width / 2, max_x), min_x),
+    max(min(math.cos(i / spiral_factor) * i * spiral_scale + height / 2, max_y), min_y),
+  ])
+
+# Set up GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+for pins in steppers:
+  for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
+
+# Set up clean exit
+def exit_handler():
+  raw_input("Press enter to return to home point")
+  target = pythagoras([width/2, height/2])
+  move_to(pos, target)
+  print("Turning off stepper motors")
+  GPIO.cleanup()
+atexit.register(exit_handler)
+
+# Init GUI
+gui = None
+try:
+  gui = tk.Tk()
+except:
+  print("GUI disabled, running headless")
+if gui:
+  print("GUI enabled, drawing stepper motor movement")
+  canvas = tk.Canvas(gui, width=width, height=height)
+  canvas.config(background=grid_background)
+  canvas.pack()
+  for x in range(0, width, grid_small):
+    canvas.create_line(x, 0, x, height, fill=grid_large_color if x % grid_large == 0 else grid_small_color)
+  for y in range(0, height, grid_small):
+    canvas.create_line(0, y, width, y, fill=grid_large_color if y % grid_large == 0 else grid_small_color)
+  for i in range(len(points) - 1):
+    canvas.create_line(points[i][0], height - points[i][1], points[i+1][0], height - points[i+1][1], fill=line_color)
+  canvas.create_line(min_x - 1, min_y - 1, max_x + 1, min_y - 1, fill=line_color, dash=(3,5), width=2)
+  canvas.create_line(min_x - 1, min_y - 1, min_x - 1, max_y + 1, fill=line_color, dash=(3,5), width=2)
+  canvas.create_line(max_x + 1, max_y + 1, max_x + 1, min_y - 1, fill=line_color, dash=(3,5), width=2)
+  canvas.create_line(max_x + 1, max_y + 1, min_x - 1, max_y + 1, fill=line_color, dash=(3,5), width=2)
+  gui.update()
+  raw_input("Press enter to start plotser...")
 
 # Follow points line with stepper motors, translate X,Y to BL,TL,TR,BR
 pos = pythagoras(points[0])
